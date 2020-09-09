c9bd326324baeff3c7abe2b144be0122a6e011be
Julio Aguilera <775176@cognizant.com>
2020-09-09 15:32:25 +0200

    develop: update post to put for pet actions and remove unnecesary profile

--- Pet.Game.API/Profiles/DtosProfile.cs
+++ Pet.Game.API/Profiles/DtosProfile.cs
@@ -12,11 +12,7 @@ namespace Pet.Game.API.Profiles
             CreateMap<Domain.Entities.PetType, Dtos.PetTypeDto>();
             CreateMap<Dtos.PetTypeDto, Domain.Entities.PetType>();
 
-            CreateMap<Domain.Entities.Pet, Dtos.PetResponseDto>()
-                .ForMember(dest => dest.HappinessStatus,
-                           opt => opt.MapFrom(src => src.HappinessStatus.ToString()))
-                .ForMember(dest => dest.HungrinessStatus,
-                           opt => opt.MapFrom(src => src.HungrinessStatus.ToString()));
+            CreateMap<Domain.Entities.Pet, Dtos.PetResponseDto>();
             CreateMap<Dtos.PetRequestDto, Domain.Entities.Pet>()
                 .ForMember(dest => dest.Id,
                            opt => opt.MapFrom(src => src.PetId));
